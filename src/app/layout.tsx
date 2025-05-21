8e829be70916eb912c0a156a094099e0d8dea104
4cnbologi4 <daniell9404@outlook.com>
2025-05-21 12:40:45 -0600

    add favicon and manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import { ThemeProvider } from "@/ui/theme-provider"
 import "./globals.css"
+import type { Metadata } from "next"
 
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "Funny Capybara API",
   description: "A Funny Capybara API",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico", sizes: "any" },
+      { url: "/favicon/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
@@ -14,6 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      </head>
       <body className="font-roboto min-h-screen w-full [&>*]:m-0 [&>*]:p-0">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <main className="w-full">
