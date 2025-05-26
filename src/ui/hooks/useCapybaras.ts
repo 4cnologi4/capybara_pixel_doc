475f182c157fe8221c504c10bce92067dc858000
4cnbologi4 <daniell9404@outlook.com>
2025-05-26 11:27:13 -0600

    delete unnecesary hooks folder

--- src/ui/hooks/useCapybaras.ts
+++
@@ -1,6 +0,0 @@
-﻿import { useQuery } from '@tanstack/react-query';
-import { fetchCapybaras } from '../../infrastructure/api/capybara';
-
-export function useCapybaras() {
-  return useQuery(['capybaras'], fetchCapybaras);
-}
